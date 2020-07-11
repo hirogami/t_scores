954f6848ccfe1edbef7519e9965810a4f6214b65
shibatau <shibatau@gmail.com>
2020-07-11 14:12:52 +0900

    Update summary.R

--- summary.R
+++ summary.R
@@ -4,15 +4,25 @@ library(mosaic)
 # load the data
 df0 <- read_csv("englishJapaneseScores1.csv")
 summary_all <- mosaic::dfapply(df0, inspect, select = TRUE)
-# histogram english overlaid with density curve
+
+# histogram english all overlaid with density curve
 ggplot(df0, aes(x=english)) +
   geom_histogram(aes(y=..density..),
                  binwidth=10,
                  colour="black", fill="white") +
   geom_density(alpha=.2, fill="#FF6666")
-# histogram japanese overlaid with density curve
+
+# histogram japanese all overlaid with density curve
 ggplot(df0, aes(x=japanese)) +
   geom_histogram(aes(y=..density..),
                  binwidth=10,
                  colour="black", fill="white") +
   geom_density(alpha=.2, fill="#FF6666")
+
+# overlaid histograms english by country
+ggplot(df0, aes(x=english, fill=nationality)) +
+  geom_histogram(binwidth=5, alpha=0.8, position="identity")
+
+# overlaid histograms japanese by country
+ggplot(df0, aes(x=japanese, fill=nationality)) +
+  geom_histogram(binwidth=5, alpha=0.8, position="identity")
